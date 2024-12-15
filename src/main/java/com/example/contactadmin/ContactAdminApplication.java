cf9d7aca32cf28d1bbb0e7866f026061bcd0eafe
ShiyaoGuo2021 <leon201808@sina.com>
2024-12-15 12:27:32 -0800

    SCRUM-16 init test

--- src/main/java/com/example/contactadmin/ContactAdminApplication.java
+++ src/main/java/com/example/contactadmin/ContactAdminApplication.java
@@ -2,7 +2,7 @@ package com.example.contactadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class ContactAdminApplication {
 
