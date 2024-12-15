560ea222ad09eef106e66bdf11cb5e56c520d417
Leon Guo <rational_leon@163.com>
2024-12-15 11:59:52 -0800

    setting up contactAdmin

---
+++ src/test/java/com/example/contactadmin/ContactAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
